5d7c285145d0ebb5f8d1b1365a3a682fa12be092
alessiotinaglia <alessio.tina1998@gmail.com>
2024-10-30 16:29:45 +0100

    fine esercizio bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -3,6 +3,7 @@ const teachers = [
   'Ed',
   'Fabio',
   'Phil',
+  'Ed',
   'Carlo',
   'Lewis',
   'Luca'
@@ -13,18 +14,29 @@ const teachers = [
 const reversedTeachers = teachers.reverse();
 console.log(reversedTeachers);
 
-
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = teachers;
 
+// usando for
+const longNames = [];
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i].length >= 5) {
+    longNames.push(teachers[i]);
+  }
+}
+console.log(longNames);
+
+// usando filter
+// const longNames = teachers.filter(teacher => teacher.length >= 5);
+// console.log(longNames);
+  
 // 3. Rimuovi 'Ed' dall'array teachers
+
 // usando filter
 const updatedTeachers = teachers.filter(teachers => teachers !== 'Ed');
 console.log(updatedTeachers);
 
 // usando splice
-
 // const index = teachers.indexOf('Ed');
 
 // if (index !== -1) {
